ad441a9f185faba1fd2ecfa4c5c296304774d66f
Christo Paul <50113036+christop23@users.noreply.github.com>
2023-07-11 09:39:44 +0530

    added colours

--- jupiter_installer.sh
+++ jupiter_installer.sh
@@ -1,92 +1,101 @@
 #!/bin/bash
 
+# ANSI color codes
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+DEFAULT="\033[0m"
+
 # Step 1: Create directory
+echo -e "${GREEN}Step 1: Creating directory${DEFAULT}"
 mkdir ~/jupiter_installer
 
 # Step 1.5: Change directory to jupiter_installer
-echo "Step 1.5: Changing directory to jupiter_installer"
+echo -e "${GREEN}Step 1.5: Changing directory to jupiter_installer${DEFAULT}"
 cd ~/jupiter_installer
 
 # Step 2: Install git
-echo "Step 2: Installing git"
+echo -e "${GREEN}Step 2: Installing git${DEFAULT}"
 sudo pacman -S git
 
 # Step 3: Clone paru-bin repository
-echo "Step 3: Cloning paru-bin repository"
+echo -e "${GREEN}Step 3: Cloning paru-bin repository${DEFAULT}"
 git clone https://aur.archlinux.org/paru-bin.git
 
 # Step 4: Clone jupiter repository
-echo "Step 4: Cloning jupiter repository"
+echo -e "${GREEN}Step 4: Cloning jupiter repository${DEFAULT}"
 git clone https://github.com/christop23/jupiter.git
 
 # Step 5: Change directory to paru-bin
-echo "Step 5: Changing directory to paru-bin"
+echo -e "${GREEN}Step 5: Changing directory to paru-bin${DEFAULT}"
 cd paru-bin
 
 # Step 6: Build and install paru
-echo "Step 6: Building and installing paru"
+echo -e "${GREEN}Step 6: Building and installing paru${DEFAULT}"
 makepkg -si
 
 # Step 7: Remove packages dmenu, foot, and vim
-echo "Step 7: Removing packages dmenu, foot, and vim"
+echo -e "${GREEN}Step 7: Removing packages dmenu, foot, and vim${DEFAULT}"
 paru -R dmenu foot vim
 
 # Step 8: Clean package cache
-echo "Step 8: Cleaning package cache"
+echo -e "${GREEN}Step 8: Cleaning package cache${DEFAULT}"
 paru -c
 
 # Step 9: Change directory to the home directory
-echo "Step 9: Changing directory to the home directory"
+echo -e "${GREEN}Step 9: Changing directory to the home directory${DEFAULT}"
 cd ~
 
 # Step 10: Install packages
-echo "Step 10: Installing packages"
-paru -S alacritty android-tools bat bemenu exa fd firefox firewalld gnome-themes-extra gvfs-mtp imv libnotify mako mpc mpd mpv ncmpcpp nemo nemo-fileroller neofetch neovim noto-fonts noto-fonts-emoji pacman-contrib polkit-gnome qt5-wayland reflector skim starship swaybg telegram-desktop ttf-firacode-nerd ttf-font-awesome ttf-iosevka-nerd ttf-jetbrains-mono-nerd xdg-user-dirs zathura zsh zsh-autosuggestions zsh-syntax-highlighting xdg-desktop-portal kvantum qt5ct python-i3ipc
+echo -e "${GREEN}Step 10: Installing packages${DEFAULT}"
+paru -S alacritty android-tools bat bemenu exa fd firefox firewalld gnome-themes-extra gvfs-mtp imv libnotify mako mpc mpd mpv ncmpcpp nemo nemo-fileroller neofetch neovim noto-fonts noto-fonts-emoji pacman-contrib polkit-gnome qt5-wayland reflector skim starship swaybg telegram-desktop ttf-firacode-nerd ttf-font-awesome ttf-iosevka-nerd ttf-jetbrains-mono-nerd xdg-user-dirs zathura zsh zsh-autosuggestions zsh-syntax-highlighting xdg-desktop-portal kvantum qt5ct python-i3ipc"
 
 # Step 11: Install package nwg-look-bin
-echo "Step 11: Installing package nwg-look-bin"
+echo -e "${GREEN}Step 11: Installing package nwg-look-bin${DEFAULT}"
 paru -S nwg-look-bin
 
 # Step 12: Enable and start firewalld service
-echo "Step 12: Enabling and starting firewalld service"
+echo -e "${GREEN}Step 12: Enabling and starting firewalld service${DEFAULT}"
 sudo systemctl enable --now firewalld.service
 
 # Step 13: Execute script to install Oh My Zsh
-echo "Step 13: Executing script to install Oh My Zsh"
+echo -e "${GREEN}Step 13: Executing script to install Oh My Zsh${DEFAULT}"
 set -x
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 set +x
 
 # Step 13.5: Update xdg user directories
-echo "Step 13.5: Updating xdg user directories"
+echo -e "${GREEN}Step 13.5: Updating xdg user directories${DEFAULT}"
 xdg-user-dirs-update
 
 # Step 14: Enable and start paccache timer
-echo "Step 14: Enabling and starting paccache timer"
+echo -e "${GREEN}Step 14: Enabling and starting paccache timer${DEFAULT}"
 sudo systemctl enable --now paccache.timer
 
 # Step 14.5: Modify configuration files
-echo "Step 14.5: Modifying configuration files"
+echo -e "${GREEN}Step 14.5: Modifying configuration files${DEFAULT}"
 
 if [ -f ~/.zshrc ]; then
     rm ~/.zshrc
-    echo "Deleted zshrc"
+    echo -e "${YELLOW}Deleted zshrc${DEFAULT}"
 fi
 
 if [ -f ~/.zprofile ]; then
     rm ~/.zprofile
-    echo "Deleted zprofile"
+    echo -e "${YELLOW}Deleted zprofile${DEFAULT}"
 fi
 
 if [ -f ~/.zshenv ]; then
     rm ~/.zshenv
-    echo "Deleted zshenv"
+    echo -e "${YELLOW}Deleted zshenv${DEFAULT}"
 fi
 
 if [ -d ~/.config ]; then
     rm -rf ~/.config
     mkdir ~/.config
-    echo "Cleared config folder"
+    echo -e "${YELLOW}Cleared config folder${DEFAULT}"
 fi
 
 mv ~/jupiter_installer/jupiter/.zshrc ~/
@@ -96,5 +105,5 @@ mv ~/jupiter_installer/jupiter/.config/* ~/.config/
 mkdir ~/.config/mpd/playlists
 
 # Step 15: Enable mpd.service
-echo "Step 15: Enabling mpd.service"
+echo -e "${GREEN}Step 15: Enabling mpd.service${DEFAULT}"
 systemctl --user enable mpd.service
